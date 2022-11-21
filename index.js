64d7d225490a7c85aee424e67983950e18381572
aminulhaque98 <aminulhaque419@gmail.com>
2022-11-21 23:48:02 +0530

    all basic setup and category data make

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running');
+});
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Lerning courses server running on port', port);
+})
